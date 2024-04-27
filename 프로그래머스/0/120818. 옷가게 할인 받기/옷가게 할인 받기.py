cd467c81b318ac8dd53aa1df996cd8f716d2f825
YEOWON <105044646+k-melodywon@users.noreply.github.com>
2024-04-28 01:31:02 +0800

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+def solution(price):
+    if (price>=500000):
+        return price*0.8//1
+    elif (500000>price>=300000):
+        return price*0.9//1
+    elif (300000>price>=100000):
+        return price*0.95//1
+    elif (100000>price):
+        return price
