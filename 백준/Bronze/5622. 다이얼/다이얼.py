3214dfb1d39f5f48282d1d1bc48d4945c6053f03
YEOWON <105044646+k-melodywon@users.noreply.github.com>
2026-01-10 16:18:25 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,40 @@
+import sys
+
+sentence = sys.stdin.readline()
+
+time = 0
+
+for i in sentence:
+
+    if i in 'ABC':
+
+        time += 3
+
+    elif i in 'DEF':
+
+        time += 4
+
+    elif i in 'GHI':
+
+        time += 5
+
+    elif i in 'JKL':
+
+        time += 6
+
+    elif i in 'MNO':
+
+        time += 7
+
+    elif i in 'PQRS':
+
+        time += 8
+
+    elif i in 'TUV':
+
+        time += 9
+
+    elif i in 'WXYZ':
+
+        time += 10
+print(time)
